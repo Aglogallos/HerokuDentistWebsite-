431000db1f13400059b9b4a175413602718beffd
Aglogallos <79355926+Aglogallos@users.noreply.github.com>
2022-12-17 22:13:52 +0200

    Initial Commit

--- dentist/website/urls.py
+++ dentist/website/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('service', views.service, name='service'),
-    path('pricing', views.pricing, name='pricing'),
     path('contact', views.contact, name='contact'),
-    path('blog', views.blog, name='blog'),
-    path('blog-details', views.blog_details, name='blog_details'),
+    path('faq', views.faq, name='faq'),
+    path('faq-page=2', views.faq_page2, name='faq-page=2'),
+    
+    path('blog-details', views.blog_details, name='blog_details'), #replace with jobs
+    path('jobs', views.jobs, name='jobs'),
+
+    path('appointment', views.appointment, name='appointment'),
 ]
--- dentist/website/views.py
+++ dentist/website/views.py
@@ -3,22 +3,7 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 def home(request):
-    if request.method == "POST" :
-        your_name = request.POST['your-name']
-        your_phone = request.POST['your-phone']
-        your_email = request.POST['your-email']
-        your_message = request.POST['message']
-        #send an email
-        send_mail(
-            'Mr/Ms ' + your_name + ' Kontakt form',
-            'Message from Mr/Ms : ' + your_name + '\nEmail : ' + your_email + '\nPhone : ' + your_phone + '\nMessage : ' +  your_message,
-            'your_email',
-            ['status.electronics.test.mail@gmail.com'],
-            fail_silently=False,
-        )
-        return render(request, 'pages/index.html', {'your_name': your_name})
-    else:
-        return render(request, 'pages/index.html', {})
+    return render(request, 'pages/index.html', {})
 
 def about(request):
     return render(request, 'pages/about.html', {})
@@ -26,11 +11,6 @@ def about(request):
 
 def service(request):
     return render(request, 'pages/service.html', {})
-
-
-def pricing(request):
-    return render(request, 'pages/pricing.html', {})
-
 
 def contact(request):
     if request.method == "POST" :
@@ -52,10 +32,35 @@ def contact(request):
     else:
         return render(request, 'pages/contact.html', {})
 
-
-def blog(request):
-    return render(request, 'pages/blog.html', {})
-
-
 def blog_details(request):
     return render(request, 'pages/blog-details.html', {})
+
+def jobs(request):
+    return render(request, 'pages/blog-details.html', {})
+
+def faq(request):
+    return render(request, 'pages/faq.html', {})
+
+def faq_page2(request):
+    return render(request, 'pages/faq2.html', {})
+
+def appointment(request):
+    if request.method == "POST" :
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_message = request.POST['your-message']
+        your_schedule = request.POST['your-schedule']
+
+        #send an email
+        send_mail(
+            'Mr/Ms ' + your_name + ' Kontakt form',
+            'Message from Mr/Ms : ' + your_name + '\nEmail : ' + your_email + '\nPhone : ' + your_phone + '\nKontact Schedule : ' + your_schedule + '\nMessage : ' +  your_message,
+            'your_email',
+            ['status.electronics.test.mail@gmail.com'],
+            fail_silently=False,
+        )
+        return render(request, 'pages/appointment.html', {'your_name': your_name, 'your_phone': your_phone, 'your_email': your_email, 'your_schedule' : your_schedule, 'your_message': your_message })
+    else:
+        return render(request, 'pages/index.html', {})
+
